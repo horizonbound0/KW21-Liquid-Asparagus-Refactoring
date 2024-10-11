494456eb06725ce3e1f50e418368e1430cb9aa73
Kenneth A Wagner <kenw2190@gmail.com>
2024-10-11 14:27:54 -0500

    fixed port issues for building site

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const db = require('./config/connection');
 
 const routes = require('./routes');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
 
 const server = new ApolloServer({
